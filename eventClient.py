561928988d39fd89138f3bf115b681b19ab9c60c
narmin9 <a.narmin20@gmail.com>
2017-12-13 20:48:26 +0300

    insert delete findClosest updateEvent -client side

--- eventClient.py
+++ eventClient.py
@@ -8,31 +8,56 @@ def client(port):
     c.connect(('127.0.0.1', port))
     while True:
         text = input("> ")
-        if text.split(" ")[0]=='insert':
-            lon = float(input("lon: "))
-            lat = float(input("lat: "))
-            locname = input("location name: ")
-            title = input("title: ")
-            desc = input("description(optional): ")
-            catstring = input("categories: ")
-            catlist = catstring.split(" ")
-            starttime = input("start time: ")
-            endtime = input("end time: ")
-            timetoann = input("time to announce: ")
-            newdict = {'method':'insert', 'params':{}}
-            newdict['params']['lon'] = lon
-            newdict['params']['lat'] = lat
-            newdict['params']['locname'] = locname
-            newdict['params']['title'] = title
-            newdict['params']['desc'] = desc
-            newdict['params']['catlist'] = catlist
-            newdict['params']['starttime'] = starttime
-            newdict['params']['endtime'] = endtime
-            newdict['params']['timetoann'] = timetoann
+        method = text.split(" ")[0]
+        newdict = {'method': method, 'params':{}}
+
+        if method=='insert':
+            newdict['params']['lon'] = float(input("lon: "))
+            newdict['params']['lat'] = float(input("lat: "))
+            newdict['params']['locname'] = input("location name: ")
+            newdict['params']['title'] = input("title: ")
+            newdict['params']['desc'] = input("description(optional): ")
+            newdict['params']['catlist'] = input("categories: ").split(" ")
+            newdict['params']['starttime'] = input("start time: ")
+            newdict['params']['endtime'] = input("end time: ")
+            newdict['params']['timetoann'] = input("time to announce: ")
             c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
             c.send(json.dumps(newdict).encode())
             reply = c.recv(1024)
-            print(c.getsockname(), reply)
+            print(c.getsockname(), reply.decode())
+        
+        elif method=='delete':
+            newdict['params']['ID'] = int(text.split(" ")[1])
+            c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
+            c.send(json.dumps(newdict).encode())
+            reply = c.recv(1024)
+            print(c.getsockname(), reply.decode())
+
+        elif method=='findClosest':
+            newdict['params']['lat'] = float(text.split(" ")[1])
+            newdict['params']['lon'] = float(text.split(" ")[2])
+            c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
+            c.send(json.dumps(newdict).encode())
+
+            length = int(c.recv(10))
+            reply = c.recv(length)
+            print(c.getsockname(), json.loads(reply.decode()))
+            
+        elif method=='updateEvent':
+            newdict['ID'] = int(text.split(" ")[1])
+            newdict['params']['lon'] = float(input("lon: "))
+            newdict['params']['lat'] = float(input("lat: "))
+            newdict['params']['locname'] = input("location name: ")
+            newdict['params']['title'] = input("title: ")
+            newdict['params']['desc'] = input("description(optional): ")
+            newdict['params']['catlist'] = input("categories: ").split(" ")
+            newdict['params']['starttime'] = input("start time: ")
+            newdict['params']['endtime'] = input("end time: ")
+            newdict['params']['timetoann'] = input("time to announce: ")
+            c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
+            c.send(json.dumps(newdict).encode())
+            reply = c.recv(1024)
+            print(c.getsockname(), reply.decode())
     c.close()
 
 client(20445)
