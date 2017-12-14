47d7a9fe279b78f9b7f7fc2cd352c3d7b5de16d9
narmin9 <a.narmin20@gmail.com>
2017-12-14 11:05:41 +0300

    save + attach -client side-

--- eventClient.py
+++ eventClient.py
@@ -2,8 +2,6 @@ import json
 from socket import * 
 
 def client(port):
-    # send n random request
-    # the connection is kept alive until client closes it.
     c = socket(AF_INET, SOCK_STREAM)
     c.connect(('127.0.0.1', port))
     while True:
@@ -11,7 +9,35 @@ def client(port):
         method = text.split(" ")[0]
         newdict = {'method': method, 'params':{}}
 
-        if method=='insert':
+        if method=='quit':
+            c.send('{:10d}'.format(len(method.encode())).encode())
+            c.send(method.encode())
+            reply = c.recv(1024).decode()
+            if reply=="connection closed":
+                print(reply)
+                c.close()
+                break
+
+        elif method=='save':
+            name = text.split(" ")[1]
+            newdict['params']['name'] = name
+            c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
+            c.send(json.dumps(newdict).encode())
+            reply = c.recv(1024)
+            print(reply.decode())
+            
+        elif method=='attach':
+            mapID = text.split(" ")[1]
+            if mapID=='NEW':
+                newdict['params']['ID'] = text.split(" ")[1]
+            else:
+                newdict['params']['ID'] = int(text.split(" ")[1])
+            c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
+            c.send(json.dumps(newdict).encode())
+            reply = c.recv(1024)
+            print(reply.decode())
+
+        elif method=='insert':
             newdict['params']['lon'] = float(input("lon: "))
             newdict['params']['lat'] = float(input("lat: "))
             newdict['params']['locname'] = input("location name: ")
@@ -24,14 +50,14 @@ def client(port):
             c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
             c.send(json.dumps(newdict).encode())
             reply = c.recv(1024)
-            print(c.getsockname(), reply.decode())
+            print(reply.decode())
         
         elif method=='delete':
             newdict['params']['ID'] = int(text.split(" ")[1])
             c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
             c.send(json.dumps(newdict).encode())
             reply = c.recv(1024)
-            print(c.getsockname(), reply.decode())
+            print(reply.decode())
 
         elif method=='findClosest':
             newdict['params']['lat'] = float(text.split(" ")[1])
@@ -41,7 +67,7 @@ def client(port):
 
             length = int(c.recv(10))
             reply = c.recv(length)
-            print(c.getsockname(), json.loads(reply.decode()))
+            print(json.loads(reply.decode()))
             
         elif method=='updateEvent':
             newdict['ID'] = int(text.split(" ")[1])
@@ -57,7 +83,7 @@ def client(port):
             c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
             c.send(json.dumps(newdict).encode())
             reply = c.recv(1024)
-            print(c.getsockname(), reply.decode())
+            print(reply.decode())
             
         elif method == "searchEvent":   # searchEvent,39.9,31,39.7,32.8,2017/11/27 19:00,+5 days,musical,opera
             newdict = {'method': method,'params':{}}
