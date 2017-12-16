15b306f849d91f14aff91058452aa2135f535dbf
axydor <e2099018@ceng.metu.edu.tr>
2017-12-16 16:39:11 +0300

    Fixed Now
    User was able to do list events without attaching to the map. Now, user cannot execute operations related to the map before attaching to a map.

--- eventClient.py
+++ eventClient.py
@@ -3,10 +3,12 @@ from socket import *
 from threading import Thread
 
 flag = 0
+attached = 0
 
 def client(c):
     while True:
         global flag
+        global attached
         flag = 1
         text = input("> ")
         flag = 0
@@ -20,15 +22,12 @@ def client(c):
 
         elif method=='list':
             newdict['params']['arg'] = text.split(" ")[1]
-            c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
-            c.send(json.dumps(newdict).encode())
+            if text.split(" ")[1] == 'events' and attached == 0:
+                print("YOU SHOULD FIRST ATTACH TO A MAP")
+            else:
+                c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
+                c.send(json.dumps(newdict).encode())
 
-        elif method=='save':
-            name = text.split(" ")[1]
-            newdict['params']['name'] = name
-            c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
-            c.send(json.dumps(newdict).encode())
-            
         elif method=='attach':
             try:
                 mapID = text.split(" ")[1]
@@ -38,128 +37,139 @@ def client(c):
                     newdict['params']['ID'] = int(text.split(" ")[1])
             except:
                 newdict['params']['ID'] = 'NEW'
-            c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
-            c.send(json.dumps(newdict).encode())
-
-        elif method=='detach':
-            c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
-            c.send(json.dumps(newdict).encode())
-            
-        elif method=='insert':
-            newdict['params']['lon'] = float(input("lon: "))
-            newdict['params']['lat'] = float(input("lat: "))
-            newdict['params']['locname'] = input("location name: ")
-            newdict['params']['title'] = input("title: ")
-            newdict['params']['desc'] = input("description(optional): ")
-            newdict['params']['catlist'] = input("categories: ").split(" ")
-            newdict['params']['starttime'] = input("start time: ")
-            newdict['params']['endtime'] = input("end time: ")
-            newdict['params']['timetoann'] = input("time to announce: ")
-            c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
-            c.send(json.dumps(newdict).encode())
-        
-        elif method=='delete':
-            newdict['params']['ID'] = int(text.split(" ")[1])
-            c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
-            c.send(json.dumps(newdict).encode())
-
-        elif method=='findClosest':
-            newdict['params']['lat'] = float(text.split(" ")[1])
-            newdict['params']['lon'] = float(text.split(" ")[2])
-            c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
-            c.send(json.dumps(newdict).encode())
-
-            
-        elif method=='updateEvent':
-            newdict['ID'] = int(text.split(" ")[1])
-            newdict['params']['lon'] = float(input("lon: "))
-            newdict['params']['lat'] = float(input("lat: "))
-            newdict['params']['locname'] = input("location name: ")
-            newdict['params']['title'] = input("title: ")
-            newdict['params']['desc'] = input("description(optional): ")
-            newdict['params']['catlist'] = input("categories: ").split(" ")
-            newdict['params']['starttime'] = input("start time: ")
-            newdict['params']['endtime'] = input("end time: ")
-            newdict['params']['timetoann'] = input("time to announce: ")
-            c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
-            c.send(json.dumps(newdict).encode())
-            
-        elif method == "searchAdvanced":   # searchEvent,39.9,31,39.7,32.8,2017/11/27 19:00,+5 days,musical,opera
-            newdict = {'method': method,'params':{}}
-            rect = {}
-            #rect={'lattl':39.9,'lontl':31,'latbr':39.7,'lonbr':32.8}
-            if input("Rectangle:> ") == 'None':
-                rect = None
-            else:
-                rect['lattl'] = float(input("lattl: "))
-                rect['lontl'] = float(input("lontl: "))
-                rect['latbr'] = float(input("latbr: "))
-                rect['lonbr'] = float(input("lonbr: "))
-            starttime = input("starttime: ")
-            endtime   = input("endtime: ")
-            category  = input("category: ")
-            text      = input("text: ")
-
-            if starttime  == 'None':
-                starttime = None
-            if endtime    == 'None':
-                endtime   = None
-            if category   == 'None':
-                category  = None
-            if text       == 'None':
-                text      = None
-
-            newdict['params']['rectangle'] = rect
-            newdict['params']['starttime'] = starttime
-            newdict['params']['endtime']   = endtime
-            newdict['params']['category']  = category
-            newdict['params']['text']      = text
-            c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
-            c.send(json.dumps(newdict).encode())
-
-        elif method == "searchbyCategory":
-            newdict = {'method': method,'params':{}}
-            category = input("> ") 
-            newdict['params']['category'] = category
-            c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
-            c.send(json.dumps(newdict).encode())
-
-        elif method == "searchbyText":
-            newdict = {'method': method,'params':{}}
-            text = input("Text:> ") 
-            newdict['params']['text'] = text
-            c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
-            c.send(json.dumps(newdict).encode())
-
-        elif method == "searchbyRect":
-            newdict = {'method': method, 'params' : {}}
-            rect = {}
-            rect['lattl'] = float(input("lattl: "))
-            rect['lontl'] = float(input("lontl: "))
-            rect['latbr'] = float(input("latbr: "))
-            rect['lonbr'] = float(input("lonbr: "))            
-            newdict['params']['rectangle'] = rect
-            c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
-            c.send(json.dumps(newdict).encode())            
-
-        elif method == "searchbyTime":
-            newdict = {'method': method,'params':{}}
-            starttime = input("starttime:> ") 
-            endtime = input("endtime:> ") 
-            newdict['params']['starttime'] = starttime
-            newdict['params']['endtime'] = endtime
-            c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
-            c.send(json.dumps(newdict).encode())
-
-        elif method == "watchArea":
+            attached = 1
             c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
             c.send(json.dumps(newdict).encode())
 
         else:
-            if text != '':
-                print("YOU HAVE ENTERED AN ILLEGAL OPERATION, TRY AGAIN")
-  
-        time.sleep(0.1)            
+                if attached == 1:     
+                    if method=='detach':
+                        attached = 0
+                        c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
+                        c.send(json.dumps(newdict).encode())
+                            
+                    elif method=='save':
+                        name = text.split(" ")[1]
+                        newdict['params']['name'] = name
+                        c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
+                        c.send(json.dumps(newdict).encode())
+                        
+                    elif method=='insert':
+                        newdict['params']['lon'] = float(input("lon: "))
+                        newdict['params']['lat'] = float(input("lat: "))
+                        newdict['params']['locname'] = input("location name: ")
+                        newdict['params']['title'] = input("title: ")
+                        newdict['params']['desc'] = input("description(optional): ")
+                        newdict['params']['catlist'] = input("categories: ").split(" ")
+                        newdict['params']['starttime'] = input("start time: ")
+                        newdict['params']['endtime'] = input("end time: ")
+                        newdict['params']['timetoann'] = input("time to announce: ")
+                        c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
+                        c.send(json.dumps(newdict).encode())
+                    
+                    elif method=='delete':
+                        newdict['params']['ID'] = int(text.split(" ")[1])
+                        c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
+                        c.send(json.dumps(newdict).encode())
+
+                    elif method=='findClosest':
+                        newdict['params']['lat'] = float(text.split(" ")[1])
+                        newdict['params']['lon'] = float(text.split(" ")[2])
+                        c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
+                        c.send(json.dumps(newdict).encode())
+
+                        
+                    elif method=='updateEvent':
+                        newdict['ID'] = int(text.split(" ")[1])
+                        newdict['params']['lon'] = float(input("lon: "))
+                        newdict['params']['lat'] = float(input("lat: "))
+                        newdict['params']['locname'] = input("location name: ")
+                        newdict['params']['title'] = input("title: ")
+                        newdict['params']['desc'] = input("description(optional): ")
+                        newdict['params']['catlist'] = input("categories: ").split(" ")
+                        newdict['params']['starttime'] = input("start time: ")
+                        newdict['params']['endtime'] = input("end time: ")
+                        newdict['params']['timetoann'] = input("time to announce: ")
+                        c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
+                        c.send(json.dumps(newdict).encode())
+                        
+                    elif method == "searchAdvanced":   # searchEvent,39.9,31,39.7,32.8,2017/11/27 19:00,+5 days,musical,opera
+                        newdict = {'method': method,'params':{}}
+                        rect = {}
+                        #rect={'lattl':39.9,'lontl':31,'latbr':39.7,'lonbr':32.8}
+                        if input("Rectangle:> ") == 'None':
+                            rect = None
+                        else:
+                            rect['lattl'] = float(input("lattl: "))
+                            rect['lontl'] = float(input("lontl: "))
+                            rect['latbr'] = float(input("latbr: "))
+                            rect['lonbr'] = float(input("lonbr: "))
+                            starttime = input("starttime: ")
+                            endtime   = input("endtime: ")
+                            category  = input("category: ")
+                            text      = input("text: ")
+
+                        if starttime  == 'None':
+                            starttime = None
+                        if endtime    == 'None':
+                            endtime   = None
+                        if category   == 'None':
+                            category  = None
+                        if text       == 'None':
+                            text      = None
+
+                        newdict['params']['rectangle'] = rect
+                        newdict['params']['starttime'] = starttime
+                        newdict['params']['endtime']   = endtime
+                        newdict['params']['category']  = category
+                        newdict['params']['text']      = text
+                        c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
+                        c.send(json.dumps(newdict).encode())
+
+                    elif method == "searchbyCategory":
+                        newdict = {'method': method,'params':{}}
+                        category = input("> ") 
+                        newdict['params']['category'] = category
+                        c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
+                        c.send(json.dumps(newdict).encode())
+
+                    elif method == "searchbyText":
+                        newdict = {'method': method,'params':{}}
+                        text = input("Text:> ") 
+                        newdict['params']['text'] = text
+                        c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
+                        c.send(json.dumps(newdict).encode())
+
+                    elif method == "searchbyRect":
+                        newdict = {'method': method, 'params' : {}}
+                        rect = {}
+                        rect['lattl'] = float(input("lattl: "))
+                        rect['lontl'] = float(input("lontl: "))
+                        rect['latbr'] = float(input("latbr: "))
+                        rect['lonbr'] = float(input("lonbr: "))            
+                        newdict['params']['rectangle'] = rect
+                        c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
+                        c.send(json.dumps(newdict).encode())            
+
+                    elif method == "searchbyTime":
+                        newdict = {'method': method,'params':{}}
+                        starttime = input("starttime:> ") 
+                        endtime = input("endtime:> ") 
+                        newdict['params']['starttime'] = starttime
+                        newdict['params']['endtime'] = endtime
+                        c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
+                        c.send(json.dumps(newdict).encode())
+
+                    elif method == "watchArea":
+                        c.send('{:10d}'.format(len(json.dumps(newdict).encode())).encode())
+                        c.send(json.dumps(newdict).encode())
+
+                else:
+                    if text != '':
+                        print("YOU HAVE ENTERED AN ILLEGAL OPERATION, TRY AGAIN")
+                        print("YOU SHOULD FIRST ATTACH TO A MAP")
+                    time.sleep(0.1)
+
 
 
 def clientNotifier(c):
