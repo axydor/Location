344fc0f92f27248f73f7cc44ca88c5d2b31f2b9c
narmin9 <a.narmin20@gmail.com>
2017-12-31 14:41:33 +0300

    fixed a merge error

--- eventmap/urls.py
+++ eventmap/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.listMaps, name='listMaps'),
     path('<int:mapid>/', views.listEvents, name='listEvents'),
-    path('<int:mapid>/insertEvent',views.insertEvent,name='insertEvent'),
+    path('<int:mapid>/insertEvent/',views.insertEvent,name='insertEvent'),
 ]
